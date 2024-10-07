0b59e2e903ce8f133f30ce91786e2938acc92d18
dawisobi <sobieski.dawid@wp.pl>
2024-10-08 00:06:43 +0200

    Removed unused import directives

--- app/src/main/java/com/example/workouttracker/WorkoutTrackerApp.kt
+++ app/src/main/java/com/example/workouttracker/WorkoutTrackerApp.kt
@@ -10,13 +10,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
