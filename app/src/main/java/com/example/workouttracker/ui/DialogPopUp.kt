1d95c061ca13c9a530533935b82c5a280d237129
dawisobi <sobieski.dawid@wp.pl>
2024-10-18 02:04:44 +0200

    Added ViewModel for the AddExerciseDialog

--- app/src/main/java/com/example/workouttracker/ui/DialogPopUp.kt
+++ app/src/main/java/com/example/workouttracker/ui/DialogPopUp.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,20 +36,42 @@ fun AddExerciseDialog(
             shape = RoundedCornerShape(16.dp)
         ) {
             Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize().padding(16.dp),
+                verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "Add Exercise", textAlign = TextAlign.Center)
+                AddExerciseContent()
             }
         }
     }
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun AddExerciseDialogPreview(){
-//    WorkoutTrackerTheme(dynamicColor = false) {
-//        AddExerciseDialog()
-//    }
-//}
+@Composable
+fun AddExerciseContent() {
+    Column {
+        Text(
+            text = "Add Exercise",
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.SemiBold
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = { Text("Search") },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+        )
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun AddExerciseDialogPreview(){
+    WorkoutTrackerTheme(dynamicColor = false) {
+        AddExerciseDialog( onDismiss = { Log.d("AddExerciseDialog", "onDismiss") } )
+    }
+}
